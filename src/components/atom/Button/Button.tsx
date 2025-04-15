0907ab020103c15539fd93ce44ba6e2ba72d7b7e
spaceman8888 <bobo920524@gmail.com>
2025-04-15 23:34:57 +0900

    feat : 폴더 구조 정리

---
+++ src/components/atom/Button/Button.tsx
@@ -0,0 +1,8 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+export default function Button({ children, onClick }: ButtonProps) {
+  return <button onClick={onClick}>{children}</button>;
+}
